66e5b5acf419f7d0492f95d45b7b6cd4bfa1e970
Yiftach19 <77735290+Yiftach19@users.noreply.github.com>
2021-01-20 20:37:38 +0200

    sprint1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 const MINE = '💣';
 
 
-var gMinePos;
+
  var gBoard = {
     minesAroundCount: 4,
     isShown: true,
@@ -12,21 +12,34 @@ var gMinePos;
 };
 
 
-debugger;
+
 buildBoard()
 function buildBoard() {
-  var board = [];
-  for (var i = 0; i < 4; i++) {
-    board[i] = [];
-    for (var j = 0; j < 4; j++) {
-      board[i][j] = gBoard;
+    var board = [];
+    for (var i = 0; i < 4; i++) {
+        board[i] = [];
+        for (var j = 0; j < 4; j++) {
+        board[i][j] = gBoard;
+        }
     }
-  }
-  board[gMinePos.i][gMinePos.j].gameElement = MINE
-  board[2][1] = MINE
-  board[0][3] = MINE
+    board[2][1] = MINE;
+    board[0][3] = MINE;
 
-  console.table(board);
+    console.table(board);
     return board;
+}
+
+
+function renderBoard(board){
+    var strHtml = '';
+    for( var i = 0; i < board.length; i++){
+        for(var j = 0; j < board[0].length; j++){
+            var currCell = board[i][j]
+
+        }
+    }
+
+
 
 }
+ 
